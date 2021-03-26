f083087d6b7b8dbd665ac3bce866b68ba4c2689d
jitiendran <jitiendranjiji2000@gmail.com>
2021-03-26 09:53:18 +0530

    Added notification

--- src/app/student/header/header.component.ts
+++ src/app/student/header/header.component.ts
@@ -1,3 +1,4 @@
+import { StudentserviceService } from './../studentservice.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -8,15 +9,24 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   user: { Rid: number; Name: string } = { Rid: 0, Name: '' };
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private stud: StudentserviceService
+  ) {}
 
   ngOnInit(): void {
     this.user = {
       Rid: this.route.snapshot.params['id'],
       Name: this.route.snapshot.params['name'],
     };
+    this.stud.user = this.user;
   }
   onNotification() {
+    this.stud.getNotification(this.user.Rid).subscribe(
+      (res) => console.log(res),
+      (error) => console.error(error)
+    );
     this.router.navigate(['Notifications'], { relativeTo: this.route });
   }
 }
---
+++ src/app/student/studentservice.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentserviceService {
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  notificationUrl = '';
+  user: { Rid: number; Name: string } = { Rid: 0, Name: '' };
+  getNotification(Id: number) {
+    return this.http.post(this.notificationUrl, Id);
+  }
+}
